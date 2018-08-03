2cabf73459efd597e3af33231f9154c2078c8f5b
Rohan Katariya <Rohan.Katariya@emtecinc.com>
2018-08-03 18:41:50 +0530

    Added refference projects

---
+++ DemoApplication/Repository/EmployeeCommandRepository.cs
@@ -0,0 +1,41 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class EmployeeCommandRepository
+    {
+        public void SaveEmployee(Employees emp)
+        {
+            DbClass.ExecuteNonQuery("InsertEmployeeData", System.Data.CommandType.StoredProcedure,
+                                    new SqlParameter("@EmployeeName", emp.EmployeeName),
+                                    new SqlParameter("@Deparmtent", emp.Department),
+                                    new SqlParameter("@EmployeeTypeId", emp.EmployeeTypeId),
+                                    new SqlParameter("@HourlyPay", emp.HourlyPay),
+                                    new SqlParameter("@Bonusinc", emp.Bonusinc)
+                                    ); 
+        }
+        public void UpdateEmployee(Employees emp)
+        {
+            DbClass.ExecuteNonQuery("UpdateEmployeeData", System.Data.CommandType.StoredProcedure,
+                                    new SqlParameter("@EmployeeId", emp.EmployeeId),
+                                    new SqlParameter("@EmployeeName", emp.EmployeeName),
+                                    new SqlParameter("@Deparmtent", emp.Department),
+                                    new SqlParameter("@EmployeeTypeId", emp.EmployeeTypeId),
+                                    new SqlParameter("@HourlyPay", emp.HourlyPay),
+                                    new SqlParameter("@Bonusinc", emp.Bonusinc)
+                                    );
+        }
+        public void DeleteEmployee(int empId)
+        {
+            DbClass.ExecuteNonQuery("DeleteEmployeeData", System.Data.CommandType.StoredProcedure,
+                                    new SqlParameter("@EmployeeId", empId)
+                                    );
+        }
+    }
+}
---
+++ DemoApplication/Repository/EmployeeQueryRepository.cs
@@ -0,0 +1,36 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class EmployeeQueryRepository
+    {
+        public Employees SearchResult(int EmpId)
+        {
+            SqlDataReader sdr = DbClass.ExecuteReader("SearchEmployeeData", System.Data.CommandType.StoredProcedure,
+                                                        new SqlParameter("@EmployeeId",EmpId)
+                                                     );
+            Employees emp=new Employees();
+            while(sdr.Read())
+            {
+                emp.EmployeeName = sdr["EmployeeName"].ToString();
+                emp.Department = sdr["Department"].ToString();
+                emp.EmployeeTypeId = Convert.ToInt32(sdr["EmployeeTypeId"]);
+            }
+            return emp;
+        }
+        public DataTable ShowEmployees()
+        {
+            SqlDataReader sdr = DbClass.ExecuteReader("ShowEmployeeData", System.Data.CommandType.StoredProcedure);
+            DataTable listEmployee = new DataTable();
+            listEmployee.Load(sdr);
+            return listEmployee;
+        }
+    }
+}
